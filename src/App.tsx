20ebe8addb889e55726ae13f6de3a6dcff09acad
curepotion <64453274+curepotion@users.noreply.github.com>
2025-04-11 18:46:46 +0800

    Update card design and fix category loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './routes/Home'
+import Home from './routes/home'
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
   )
-}
-
-export default App 
+} 
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cocktail-random/',
+  base: '/',
+  server: {
+    port: 3000,
+    open: true
+  }
 })
